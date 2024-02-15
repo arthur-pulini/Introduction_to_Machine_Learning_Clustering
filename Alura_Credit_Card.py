33ad46dab835a3754a43baefcbe5f31ed5f29f31
Arthur Pulini <arthurpulini@gmail.com>
2024-02-15 19:19:12 -0300

    Applying Silhouette

--- Alura_Credit_Card.py
+++ Alura_Credit_Card.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import Normalizer
 from sklearn.cluster import KMeans
+from sklearn import metrics
 
 datas = pd.read_csv("/home/arthur-pulini/Documentos/Programação/Machine learning Alura/Alura_Credit_Card/CC GENERAL.csv")
 datas.drop(columns=['CUST_ID', 'TENURE'], inplace=True) #Retirando algumas colunas que não fazem sentido ao cluster
@@ -24,3 +25,8 @@ print(values)
 #max_iter informa o número de iterações
 kmeans = KMeans(n_clusters = 5, n_init = 10, max_iter = 300)
 yPred = kmeans.fit_predict(values)
+
+#Aplicando a métrica de validação com o coeficiente de silhouette
+labels = kmeans.labels_
+silhouette = metrics.silhouette_score(values, labels, metric='euclidean')
+print(silhouette)
